2f62e830c1d85f46ac2a47d2eb0a80c81291d5e8
Gokulprasath-Kore <144587668+Gokulprasath-Kore@users.noreply.github.com>
2023-09-22 16:46:38 +0530

    Update passport-setup.js

--- passport-setup.js
+++ passport-setup.js
@@ -18,7 +18,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: "https://shopifyauth.onrender.com/google/callback",
     passReqToCallback: true
 }, (request, accessToken, refreshToken, profile, done) => {
     console.log(profile)
